b4a742d8cdd9fd5e32733f296722075b7812bb12
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-26 16:49:27 +0300

    Сделан подсчёт количесвта элементов для нового массива

--- Program.cs
+++ Program.cs
@@ -20,3 +20,23 @@ void Author()
     Console.WriteLine("E-mail: Mnement4813@yandex.ru");
     Console.WriteLine();
 }
+
+string[] ThreeCharArray(string[] userArray)
+{
+    int count = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i].Length <= 3)
+            count++;
+    }
+    Console.WriteLine($"count = {count}");
+    string[] finishArray = new string[count];
+    
+    return finishArray;
+}
+
+string[] testArray = { "hello", "2", "world", ":-)" };
+string[] testArray2 = { "1234", "1567", "-2", "computer science" };
+string[] testArray3 = { "Russia", "Denmark", "Kazan" };
+
+string[] threeCharArray = ThreeCharArray(testArray3);
